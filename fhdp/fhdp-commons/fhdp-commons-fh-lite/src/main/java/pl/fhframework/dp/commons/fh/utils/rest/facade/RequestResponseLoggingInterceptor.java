1e961f3453ea705bddd8471c127da3e32100eaef
Jerzy Pietrzykowski <jerzy.pietrzykowski@asseco.pl>
2024-10-12 10:39:23 +0200

    [fix](NO_TASK) on demand additional logging for FacadeRestTemplate

--- fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/utils/rest/facade/RequestResponseLoggingInterceptor.java
+++ fhdp/fhdp-commons/fhdp-commons-fh-lite/src/main/java/pl/fhframework/dp/commons/fh/utils/rest/facade/RequestResponseLoggingInterceptor.java
@@ -1,5 +1,6 @@
 package pl.fhframework.dp.commons.fh.utils.rest.facade;
 
+import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
@@ -10,39 +11,43 @@ import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
-//TODO: this log interceptor should be connected at  FacadeRestTemplateConfig based on external parameter
 public class RequestResponseLoggingInterceptor implements ClientHttpRequestInterceptor {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        logRequest(request, body);
-        ClientHttpResponse response = execution.execute(request, body);
-        logResponse(response);
-        return response;
-    }
-
-    private void logRequest(HttpRequest request, byte[] body) throws IOException {
-        if (log.isDebugEnabled()) {
-            log.debug("===========================request begin================================================");
-            log.debug("URI         : {}", request.getURI());
-            log.debug("Method      : {}", request.getMethod());
-            log.debug("Headers     : {}", request.getHeaders());
-            log.debug("Request body: {}", new String(body, "UTF-8"));
-            log.debug("==========================request end================================================");
+    @NonNull
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
+          throws IOException {
+        if (log.isDebugEnabled()){
+            logRequest(request, body);
+            ClientHttpResponse response = execution.execute(request, body);
+            return logResponse(response);
+        } else {
+            return execution.execute(request, body);
         }
     }
 
-    private void logResponse(ClientHttpResponse response) throws IOException {
-        if (log.isDebugEnabled()) {
-            log.debug("============================response begin==========================================");
-            log.debug("Status code  : {}", response.getStatusCode());
-            log.debug("Status text  : {}", response.getStatusText());
-            log.debug("Headers      : {}", response.getHeaders());
-            log.debug("Response body: {}", StreamUtils.copyToString(response.getBody(), Charset.defaultCharset()));
-            log.debug("=======================response end=================================================");
-        }
+    private void logRequest(HttpRequest request, byte[] body) {
+        log.debug("===========================request begin================================================");
+        log.debug("URI         : {}", request.getURI());
+        log.debug("Method      : {}", request.getMethod());
+        log.debug("Headers     : {}", request.getHeaders());
+        log.debug("Request body: {}", new String(body, StandardCharsets.UTF_8));
+        log.debug("==========================request end================================================");
+
+    }
+
+    private ClientHttpResponse logResponse(ClientHttpResponse response) throws IOException {
+        CustomBufferingClientHttpResponseWrapper bufferedResponse = new CustomBufferingClientHttpResponseWrapper(response);
+        log.debug("============================response begin==========================================");
+        log.debug("Status code  : {}", response.getStatusCode());
+        log.debug("Status text  : {}", response.getStatusText());
+        log.debug("Headers      : {}", response.getHeaders());
+        log.debug("Response body: {}", StreamUtils.copyToString(bufferedResponse.getBody(), Charset.defaultCharset()));
+        log.debug("=======================response end=================================================");
+        return bufferedResponse;
     }
 }
