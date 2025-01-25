072f7c645767d840422bbd6417d0914341d4158f
Jerzy Pietrzykowski <jerzy.pietrzykowski@asseco.pl>
2025-01-25 20:58:12 +0100

    fix merge

--- fhCoreLite/fhPersistenceLite/src/main/java/pl/fhframework/fhPersistence/core/repository/BaseRepositoryImpl.java
+++ fhCoreLite/fhPersistenceLite/src/main/java/pl/fhframework/fhPersistence/core/repository/BaseRepositoryImpl.java
@@ -39,7 +39,7 @@ public class BaseRepositoryImpl<T, ID extends Serializable>
             return super.save(entity);
         }
 
-        entityManager.merge(entity);
+        entity = entityManager.merge(entity);
         return entity;
     }
 }
