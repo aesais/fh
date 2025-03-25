deca21a173b769056f88ced2cbba92378242deb9
Pawel.Domanski <pawel.domanski@asseco.pl>
2025-03-25 15:43:03 +0100

    [feature](NO_TASK) ForceLogoutEvent change (deleted auto redirect to login page page, added dialog with info and buton with redirect to login page) - Frontend :  4.10.6-PL-SNAPSHOT-1742910916394

--- packages/components/core-lite/source/Events/ForcedLogoutEvent.ts
+++ packages/components/core-lite/source/Events/ForcedLogoutEvent.ts
@@ -4,6 +4,10 @@ import {Util} from "../Util";
 import getDecorators from "inversify-inject-decorators";
 import {FhContainer} from "../FhContainer";
 import {ConversationHandler} from "../Socket/ConversationHandler";
+import {Connector} from "../Socket/Connector";
+import {ForcedLogutEventPL} from "./i18n/ForcedLogutEvent.pl";
+import {ForcedLogutEventEN} from "./i18n/ForcedLogoutEvent.en";
+
 let { lazyInject } = getDecorators(FhContainer);
 
 @injectable()
@@ -11,12 +15,27 @@ class ForcedLogoutEvent extends BaseEvent {
     @lazyInject("Util")
     protected util: Util;
 
+    @lazyInject("Connector")
+    protected connector: Connector;
+
     @lazyInject("ConversationHandler")
     protected conversationHandler: ConversationHandler;
 
+    constructor() {
+        super();
+        this.i18n.registerStrings('pl', ForcedLogutEventPL);
+        this.i18n.registerStrings('en', ForcedLogutEventEN);
+    }
+
     public fire(data) {
         this.conversationHandler.clearData();
-        window.location.href = this.util.getPath('autologout?reason=') + data.reason;
+        this.connector.close();
+        this.util.showDialog(this.i18n.__('forcedLogout.title'),
+            this.i18n.__('forcedLogout.message'),
+            this.i18n.__('forcedLogout.button'),
+            'btn-primary',
+            function() { location.reload(); }
+        );
     }
 }
 
---
+++ packages/components/core-lite/source/Events/i18n/ForcedLogutEvent.pl.ts
@@ -0,0 +1,5 @@
+export let ForcedLogutEventPL = {
+  "forcedLogout.title": "Wylogowanie",
+  "forcedLogout.message": "Zostałeś wylogowany, przejdź na stronę logowania aby ponownie sie zalogować.",
+  "forcedLogout.button": "Strona logowania",
+};
